9135c83fb29322554aefdfe97bb1c8fc88d1b065
westmoso <westmoso@gmail.com>
2021-01-01 11:32:02 -0500

    added date picker to signup page. coded header component and styling, which covers/obscures signup/in/out form. disabling header in app.js for now.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, } from "react-router-dom";
 import Home from "./components/Home";
+// import Header from "./components/Header"
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
 import { AuthProvider } from "./auth/Auth";
@@ -12,11 +13,14 @@ const App = () => {
   return (
     <AuthProvider>
       <Router>
-        <div>
-          <PrivateRoute exact path="/" component={Home} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/signup" component={SignUp} />
-        </div>
+        <Switch>
+          {/* <Header /> */}
+          <div>
+            <PrivateRoute exact path="/" component={Home} />
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={SignUp} />
+          </div>
+        </Switch>
       </Router>
     </AuthProvider>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react'
+import PersonIcon from '@material-ui/icons/Person';
+import ForumIcon from '@material-ui/icons/Forum';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import '../styling/Header.css';
+import { Link, useHistory } from 'react-router-dom';
+import { slide as Menu } from 'react-burger-menu';
+
+function Header({ backButton }) {
+    const history = useHistory();
+    const [menuOpen, setMenuOpen] = useState(false);
+    console.log('menuOpen is:', menuOpen);
+    const handleStateChange = (state) => {
+        console.log('reached handleStateChange in header');
+        setMenuOpen(state.isOpen);
+    }
+    const closeMenu = () => {
+        console.log('reached closeMenu');
+        setMenuOpen(false);
+    }
+
+
+    return (
+        <div className="header">
+            {(backButton) ?
+                (<IconButton onClick={() => history.goBack()}>
+                    <ArrowBackIosIcon fontSize="large" />
+                </IconButton>)
+                : (<Menu
+                    isOpen={menuOpen}
+                    onStateChange={(state) => handleStateChange(state)}>
+                    <a className="menuItem" onClick={() => closeMenu()} href="/" >Home</a>
+                    <a className="menuItem" onClick={() => closeMenu()} href="/profile" >Profile</a>
+                    <a className="menuItem" onClick={() => closeMenu()} href="/chat">Chat</a>
+                    <a className="menuItem" onClick={() => closeMenu()} href="/login">Login</a>
+                </Menu>
+                )}
+            <Link to="/">
+                <img
+                    className="tinder_logo"
+                    src="https://i0.wp.com/www.vectorico.com/wp-content/uploads/2018/02/Tinder-Logo.png"
+                    alt="tinder_logo" />
+            </Link>
+            <Link to="/profile">
+                <IconButton>
+                    <PersonIcon fontSize="large" />
+                </IconButton>
+            </Link>
+            <Link to="/chat">
+                <IconButton>
+                    <ForumIcon fontSize="large" />
+                </IconButton>
+            </Link>
+        </div>
+    )
+}
+
+export default Header
