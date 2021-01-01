a0771b67920cbaae8e9b0dc46cec404ebe5e7694
westmoso <westmoso@gmail.com>
2021-01-01 11:44:14 -0500

    coded swipe button component and styling, added to app.js. need to change main page to home and not login/sign up. sign up form is always displaying, will replace with user cards.

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import Home from "./components/Home";
 import Header from "./components/Header"
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
+import SwipeButton from './components/SwipeButton';
 import { AuthProvider } from "./auth/Auth";
 import PrivateRoute from "./auth/PrivateRoute";
+
 
 
 const App = () => {
@@ -19,6 +21,7 @@ const App = () => {
             <PrivateRoute exact path="/" component={Home} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/signup" component={SignUp} />
+            <SwipeButton />
           </div>
         </Switch>
       </Router>
@@ -27,3 +30,5 @@ const App = () => {
 };
 
 export default App;
+
+ //replace lines 22 and 23 with the cards so it's not always showing
