8c0a17057ed58bafc0913ca5fef79a75e1df0b07
westmoso <westmoso@gmail.com>
2021-01-05 19:44:10 -0500

    created star rating component and styling. added star rating to resources page.

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 import LeafMap from './LeafMap';
+import StarRate from "./StarRate"
 
 export default function Resources() {
     return (
         <div>
             Resources Page
             <LeafMap />
+            <StarRate />
         </div>
     )
 }
---
+++ src/components/StarRate.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import '../styling/StarRate.css';
+
+const Star = ({ selected = false, onClick = f => f }) => (
+    <div className={selected ? 'star selected' : 'star'}
+        onClick={onClick} />
+);
+
+const StarRate = ({ totalStars }) => {
+    const [starsSelected, selectStar] = useState(0);
+    return (
+        <div className="star-rating">
+            {[...Array(totalStars)].map((n, i) => (
+                <Star
+                    key={i}
+                    selected={i < starsSelected}
+                    onClick={() => selectStar(i + 1)}
+                />
+            ))}
+            <p>
+                {starsSelected} of {totalStars} stars
+      </p>
+        </div>
+    )
+}
+
+export default StarRate
